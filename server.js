8ff8910eafc41a7efd35059b30a7805391d55c6e
Dimm Krasnikov <krasnikov.dimm@gmail.com>
2020-11-23 17:05:57 +0300

    Add API routes for transactions model

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const {port, mongoURL} = require('./config')
+const transactionsRoutes = require('./routes/transactions')
 
-mongoose.connect(mongoURL, {
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
+
+mongoose
+    .connect(mongoURL, {
     useNewUrlParser: true,
     userCreateIndex: true,
     useUnifiedTopology: true
 
-})
-.then(() => console.log('MongoDB dstabase is connected'))
-.catch((err) => console.log(err))
+    })
+    .then(() => console.log('MongoDB dstabase is connected'))
+    .catch((err) => console.log(err))
+
+app.use('/api/transactions', transactionsRoutes)
 
 app.get('/', (req, res) => res.send('hello world'))
 
